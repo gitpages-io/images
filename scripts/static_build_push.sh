dd6a529817bf29f658ef5f78de42daba59a27e0a
dgellow <samuel.elborai@gmail.com>
2017-03-04 12:29:44 +0100

    rename env vars to OWNER_ID and PROJECT_ID

--- scripts/static_build_push.sh
+++ scripts/static_build_push.sh
@@ -7,15 +7,15 @@ BUILD_DIR="${BUILD_DIR:?Env var BUILD_DIR should be set}"
 
 REPO_NAME="${REPO_NAME:?Env var REPO_NAME should be set}"
 REPO_URL="${REPO_URL:?Env var REPO_URL should be set}"
-USER_NICK="${USER_NICK:?Env var USER_NICK should be set}"
-PROJECT_NAME="${PROJECT_NAME:?Env var PROJECT_NAME should be set}"
+OWNER_ID="${OWNER_ID:?Env var OWNER_ID should be set}"
+PROJECT_ID="${PROJECT_ID:?Env var PROJECT_ID should be set}"
 
 MINIO_HOST="${MINIO_HOST:?Env var MINIO_HOST should be set}"
 MINIO_ACCESSKEY="${MINIO_ACCESSKEY:?Env var MINIO_ACCESSKEY should be set}"
 MINIO_SECRETKEY="${MINIO_SECRETKEY:?Env var MINIO_SECRETKEY should be set}"
 MINIO_BUCKET="${MINIO_BUCKET:?Env var MINIO_BUCKET should be set}"
 
-PUSH_TARGET="minio/$MINIO_BUCKET/$USER_NICK/$PROJECT_NAME/"
+PUSH_TARGET="minio/$MINIO_BUCKET/$OWNER_ID/$PROJECT_ID/"
 
 # Get sources
 git clone $REPO_URL
